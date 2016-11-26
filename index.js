36128dbf9ce7f262047fd6cb6bfb3b5ed663051f
lblfantasy <lebfantasy@gmail.com>
2016-11-26 16:14:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+   push: {
+    android: {
+      senderId: '843186377993', // The Sender ID of GCM
+      apiKey: 'AIzaSyAqy7-ZCGhnvnM4M6fZUJMLuH_QEvvKZsQ' // The Server API Key of GCM
+    }
+    
+  },
   fileKey: process.env.FILE_KEY || 'b49e7a8a-3b2a-49ae-9e34-b010a7540e52',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
