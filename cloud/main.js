898a1010f72912ceb0391ff44ea4d5fa214b7a72
lblfantasy <lebfantasy@gmail.com>
2016-11-27 20:06:52 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -911,7 +911,7 @@ Parse.Cloud.define('computeScoreRound', function(req, res) {
  
   var userQuery = new Parse.Query('_User');
 
-	userQuery.equalTo('username','ramy89');
+	userQuery.equalTo('username','kevinnn');
   
   
   
